481abbbf648d910f3f85e7baedaf9a695f327649
Raymond Cabrera <rjgcengr@gmail.com>
2018-08-12 21:14:15 -0700

    implement mapStateToProps() and renderPosts() fns in posts_index.js

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -7,14 +8,31 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    console.log(this.props.posts);
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
 // shortcut to wiring up action creator (rather than defining mapDispatchToProps()). one example to use separate fns: if you want to do some computation on how you want to call the action reactor
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
