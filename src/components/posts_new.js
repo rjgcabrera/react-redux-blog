832eb39417dac07d9f6546c01b91ac850a53c9d8
Raymond Cabrera <rjgcengr@gmail.com>
2018-08-14 17:30:19 -0700

    Add conditional error rendering to form input errors

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,29 +3,39 @@ import { Field, reduxForm } from 'redux-form'; // reduxForm similar to connect()
 
 class PostsNew extends Component {
   renderField(field) { // field arg contains event handlers that need to be wired up to our jsx
+    const { meta: { touched, error } } = field;
+    const className = `form-group${touched && error ? ' has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
   renderTextArea(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group${touched && error ? ' has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <textarea
           className="form-control"
           type="textarea"
           {...field.input}
         />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
